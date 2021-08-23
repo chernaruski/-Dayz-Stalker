660e52ca387f42bb0e83cc635c57cb2cfcd5b7b1
Crevan <a.siverskii@gmail.com>
2021-08-23 20:23:36 +0300

    add new backpacks

--- StrelokLegacy/backpacks/config.cpp
+++ StrelokLegacy/backpacks/config.cpp
@@ -4,16 +4,16 @@ class CfgPatches
 	{
 		units[]=
 		{
-			"SL_MountainBag_100_Green",
+			"SL_MountainBag_120_Green",
 			"SL_MountainBag_250_Green",
 			"SL_MountainBag_500_Green",
 			"SL_MountainBag_100_Black",
 			"SL_MountainBag_250_Black",
 			"SL_MountainBag_500_Black",
-			"SL_MountainBag_100_Camo",
+			"SL_MountainBag_120_Camo",
 			"SL_MountainBag_250_Camo",
 			"SL_MountainBag_500_Camo",
-			"SL_MountainBag_100_BlackCamo",
+			"SL_MountainBag_120_BlackCamo",
 			"SL_MountainBag_250_BlackCamo",
 			"SL_MountainBag_500_BlackCamo"
 		};
@@ -28,8 +28,18 @@ class CfgPatches
 };
 class CfgVehicles
 {
+	class CoyoteBag_Green;
+	class SL_CoyoteBag_Green_80: CoyoteBag_Green;
+
+	class AliceBag_Green;
+	class AliceBag_Black;
+	class AliceBag_Camo;
+	class SL_AliceBag_Green_100: AliceBag_Green;
+	class SL_AliceBag_Black_100: AliceBag_Black;
+	class SL_AliceBag_Camo_100: AliceBag_Camo;
+
 	class MountainBag_ColorBase;
-	class SL_MountainBag_100_Green: MountainBag_ColorBase
+	class SL_MountainBag_120_Green: MountainBag_ColorBase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -42,7 +52,7 @@ class CfgVehicles
 			"WalkieTalkie"
 		};
 		itemSize[]={3,5};
-		itemsCargoSize[]={10,10};
+		itemsCargoSize[]={10,12};
 		hiddenSelectionsTextures[]=
 		{
 			"\StrelokLegacy\backpacks\data\sl_mountain_green_co.paa",
@@ -96,7 +106,8 @@ class CfgVehicles
 			"\StrelokLegacy\backpacks\data\sl_mountain_green_co.paa"
 		};
 	};
-	class SL_MountainBag_100_Black: MountainBag_ColorBase
+
+	class SL_MountainBag_120_Black: MountainBag_ColorBase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -109,7 +120,7 @@ class CfgVehicles
 			"WalkieTalkie"
 		};
 		itemSize[]={3,5};
-		itemsCargoSize[]={10,10};
+		itemsCargoSize[]={10,12};
 		hiddenSelectionsTextures[]=
 		{
 			"\StrelokLegacy\backpacks\data\sl_mountain_black_co.paa",
@@ -163,7 +174,8 @@ class CfgVehicles
 			"\StrelokLegacy\backpacks\data\sl_mountain_black_co.paa"
 		};
 	};
-	class SL_MountainBag_100_Camo: MountainBag_ColorBase
+
+	class SL_MountainBag_120_Camo: MountainBag_ColorBase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -176,7 +188,7 @@ class CfgVehicles
 			"WalkieTalkie"
 		};
 		itemSize[]={3,5};
-		itemsCargoSize[]={10,10};
+		itemsCargoSize[]={10,12};
 		hiddenSelectionsTextures[]=
 		{
 			"\StrelokLegacy\backpacks\data\sl_mountain_camo_co.paa",
@@ -206,7 +218,6 @@ class CfgVehicles
 			"\StrelokLegacy\backpacks\data\sl_mountain_camo_co.paa"
 		};
 	};
-	
 	class SL_MountainBag_500_Camo: MountainBag_ColorBase
 	{
 		scope=2;
@@ -231,7 +242,8 @@ class CfgVehicles
 			"\StrelokLegacy\backpacks\data\sl_mountain_camo_co.paa"
 		};
 	};
-	class SL_MountainBag_100_BlackCamo: MountainBag_ColorBase
+
+	class SL_MountainBag_120_BlackCamo: MountainBag_ColorBase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -244,7 +256,7 @@ class CfgVehicles
 			"WalkieTalkie"
 		};
 		itemSize[]={3,5};
-		itemsCargoSize[]={10,10};
+		itemsCargoSize[]={10,12};
 		hiddenSelectionsTextures[]=
 		{
 			"\StrelokLegacy\backpacks\data\sl_mountain_blackcamo_co.paa",
@@ -274,7 +286,6 @@ class CfgVehicles
 			"\StrelokLegacy\backpacks\data\sl_mountain_blackcamo_co.paa"
 		};
 	};
-	
 	class SL_MountainBag_500_BlackCamo: MountainBag_ColorBase
 	{
 		scope=2;
